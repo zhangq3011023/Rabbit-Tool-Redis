49e49d1fcd4375b72a1931266d10df8586e98e72
WH531276 <309347117@qq.com>
2020-12-19 18:07:14 +0800

    提交一个web的demo程序

--- src/main/java/com/xinyiglass/rabbit/toolredis/ToolRedisApplication.java
+++ src/main/java/com/xinyiglass/rabbit/toolredis/ToolRedisApplication.java
@@ -3,7 +3,7 @@ package com.xinyiglass.rabbit.toolredis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages="com.xinyiglass.demotest")
 public class ToolRedisApplication {
 
 	public static void main(String[] args) {
